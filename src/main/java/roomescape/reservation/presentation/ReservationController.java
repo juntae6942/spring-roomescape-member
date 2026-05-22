8289c5e0df6d03e27f5ca44026e1ff8da92848da
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-05-22 10:43:49 +0900

    refactor: 파라미터 불변 설정

--- src/main/java/roomescape/reservation/presentation/ReservationController.java
+++ src/main/java/roomescape/reservation/presentation/ReservationController.java
@@ -27,7 +27,7 @@ public class ReservationController {
     private final ReservationService reservationService;
 
     @GetMapping
-    public ResponseEntity<List<ReservationResponse>> getReservationsByName(@RequestParam String username) {
+    public ResponseEntity<List<ReservationResponse>> getReservationsByName(@RequestParam final String username) {
         List<ReservationResponse> responses = reservationService.getReservationsByName(username)
                 .stream()
                 .map(ReservationResponse::from)
@@ -36,19 +36,19 @@ public class ReservationController {
     }
 
     @PostMapping
-    public ResponseEntity<ReservationResponse> createReservation(@Valid @RequestBody ReservationRequest request) {
+    public ResponseEntity<ReservationResponse> createReservation(@Valid @RequestBody final ReservationRequest request) {
         ReservationResponse response = ReservationResponse.from(reservationService.addReservation(request.toCommand()));
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable Long id, @RequestParam String username) {
+    public ResponseEntity<Void> deleteReservation(@PathVariable final Long id, @RequestParam final String username) {
         reservationService.cancelReservation(id, username);
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<ReservationResponse> changeReservation(@PathVariable Long id, @Valid @RequestBody ReservationChangeRequest request) {
+    public ResponseEntity<ReservationResponse> changeReservation(@PathVariable final Long id, @Valid @RequestBody final ReservationChangeRequest request) {
         ReservationResponse response = ReservationResponse.from(reservationService.changeReservation(id, request.toCommand()));
         return ResponseEntity.ok(response);
     }
